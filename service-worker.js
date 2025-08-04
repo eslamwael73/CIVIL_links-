7a071d5fa29e696cffe206028ded27e389c27e00
eslamwael73 <IslamWael_23244140@gi.edu.eg>
2025-08-05 00:11:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = 'civilfiles-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/main.js',
+  '/CIVIL_links-/',
+  '/CIVIL_links-/index.html',
+  '/CIVIL_links-/style.css',
+  '/CIVIL_links-/main.js',
+  '/CIVIL_links-/icon.png',
   'https://unpkg.com/lucide@0.441.0/dist/umd/lucide.min.js',
   'https://cdn.jsdelivr.net/npm/toastify-js',
   'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js',
   'https://cdn.jsdelivr.net/npm/moment-hijri@2.1.2/moment-hijri.min.js',
   'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap',
-  'https://i.postimg.cc/Jhr0BFT4/Picsart-25-07-20-16-04-51-889.png',
   'https://i.postimg.cc/Y9LHL2xH/Picsart-25-07-22-18-01-32-565.png'
 ];
 
