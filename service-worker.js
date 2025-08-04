b5f0e729a182c02c564040ab9a8e98b830855124
eslamwael73 <IslamWael_23244140@gi.edu.eg>
2025-08-04 23:43:53 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,15 @@ const CACHE_NAME = 'civilfiles-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
+  '/style.css',
   '/main.js',
-  '/icon.png'
+  'https://unpkg.com/lucide@0.441.0/dist/umd/lucide.min.js',
+  'https://cdn.jsdelivr.net/npm/toastify-js',
+  'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js',
+  'https://cdn.jsdelivr.net/npm/moment-hijri@2.1.2/moment-hijri.min.js',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap',
+  'https://i.postimg.cc/Jhr0BFT4/Picsart-25-07-20-16-04-51-889.png',
+  'https://i.postimg.cc/Y9LHL2xH/Picsart-25-07-22-18-01-32-565.png'
 ];
 
 self.addEventListener('install', event => {
