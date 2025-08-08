6f67d23409fd1d40cda8cb794c278e9c09d9db74
eslamwael73 <IslamWael_23244140@gi.edu.eg>
2025-08-09 02:34:00 +0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,9 @@
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+// Filename: firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
+// قم بتهيئة تطبيق Firebase هنا باستخدام بيانات مشروعك
+const firebaseConfig = {
   apiKey: "AIzaSyAADlFaE-Qmp19P2wIsnZdjWhDmkjEJm8A",
   authDomain: "eslam-api-5a47a.firebaseapp.com",
   projectId: "eslam-api-5a47a",
@@ -13,21 +11,21 @@ firebase.initializeApp({
   messagingSenderId: "1001488651880",
   appId: "1:1001488651880:web:2ca301f5ea7e23e0c38ddd",
   measurementId: "G-4TMM1SBG3B"
-});
+};
 
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
-const messaging = firebase.messaging();
+const app = firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging(app);
 
 messaging.onBackgroundMessage((payload) => {
   console.log(
     '[firebase-messaging-sw.js] Received background message ',
     payload
   );
-  // Customize notification here
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png' // يمكنك تغيير الأيقونة هنا
+    icon: 'https://eslamwael-api-arbic.netlify.app/icon.png' // تم تحديث الرابط ليكون مباشرًا
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
